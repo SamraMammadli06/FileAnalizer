de70532b8b121b3145dc7676ca4e45778362bef2
Samra Mammadli <memmedlisemra6@gmail.com>
2023-07-11 00:35:14 +0400

    Add Logic for ProgressBar and Counters

--- ViewModels/FileViewModel.cs
+++ ViewModels/FileViewModel.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 using System.Windows;
 
 namespace FileAnalizer.ViewModels;
-public class FileViewModel:ViewModelBase
+public class FileViewModel : ViewModelBase
 {
     private readonly IMessenger messenger;
     public FileViewModel(IMessenger messenger)
@@ -49,6 +49,27 @@ public class FileViewModel:ViewModelBase
         get { return progressValue; }
         set => base.PropertyChange(out progressValue, value);
     }
+
+    private int symbolsCount;
+
+    public int SymbolsCount
+    {
+        get => symbolsCount;
+        set => base.PropertyChange(out symbolsCount, value);
+    }
+    private int wordsCount;
+    public int WordsCount
+    {
+        get => wordsCount;
+        set => base.PropertyChange(out wordsCount, value);
+    }
+
+    private int sentencesCount;
+    public int SentencesCount
+    {
+        get => sentencesCount;
+        set => base.PropertyChange(out sentencesCount, value);
+    }
     #endregion
 
     #region Commands
@@ -59,7 +80,7 @@ public class FileViewModel:ViewModelBase
     public MyCommand OpenCommand
     {
         get => this.openCommand ??= new MyCommand(
-            action: () => OpenFile(),
+            action: () =>  OpenFile(),
             predicate: () => true);
         set => base.PropertyChange(out this.openCommand, value);
     }
@@ -75,47 +96,72 @@ public class FileViewModel:ViewModelBase
     #region Methods
     void OpenFile()
     {
-        
-        if(string.IsNullOrEmpty(selectedItem))
+
+        if (string.IsNullOrEmpty(selectedItem))
+        {
+            return ;
+        }
+        var thread = new Thread(() => {
+            try
+            {
+                FileText = string.Empty;
+                using (StreamReader sr = new StreamReader(@$"{folderAppData}\{selectedItem}"))
+                {
+                    this.ProgressValue = 0;
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        this.FileText += line;
+                    }
+                    this.WordsCount = this.FileText.Split(' ', '\t', '\n').Length;
+                    this.SymbolsCount = this.FileText.Length;
+                    this.SentencesCount = this.FileText.Split('.', '!', '?').Length;
+                    for (ProgressValue = wordsCount; ProgressValue <= 100; ProgressValue += wordsCount)
+                    {
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+            catch {
+                MessageBox.Show("No such file in directory");
+            }
+
+        });
+        thread.Start();
+    }
+
+    void SaveChanges()
+    {
+        if (string.IsNullOrEmpty(selectedItem))
         {
             return;
         }
-        var thread = new Thread(() => {
-                try
-                { 
-                    FileText = string.Empty;
-                    using (StreamReader sr = new StreamReader(@$"{folderAppData}\{selectedItem}"))
-                    {
-                        string line;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            this.FileText += line;
-                        }
-                    }
-                    return;
-                    
-                } 
-                catch {
-                    MessageBox.Show("No such file in directory");
-                }
-            
-        });
-        thread.Start();
+        
+        var thread = new Thread(() =>
+        {
 
-    }
-    void SaveChanges()
-    {
-        var thread = new Thread(() => { 
-            using (StreamWriter sw = new StreamWriter(@$"{folderAppData}\{selectedItem}")) 
+            try
             {
-                sw.WriteLine(this.FileText);
+                
+                using (StreamWriter sw = new StreamWriter(@$"{folderAppData}\{selectedItem}"))
+                {
+                    sw.WriteLine(this.FileText);
+                }
             }
-        }
-        );
-        thread.Start();
+            catch
+            {
+                MessageBox.Show("No such file in directory");
+            }
+        });
 
+        if (this.ProgressValue >= 100)
+        {
+            thread.Start();
+        }
     }
+}
+ 
     #endregion
 
-}
+
 
