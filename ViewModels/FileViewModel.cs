b446d8ae5e848e1c26598070f43eeeb2262fda7c
Samra Mammadli <memmedlisemra6@gmail.com>
2023-07-10 16:58:46 +0400

    Add Commands

--- ViewModels/FileViewModel.cs
+++ ViewModels/FileViewModel.cs
@@ -1,11 +1,15 @@
-﻿using FileAnalizer.Services;
+﻿using FileAnalizer.Messager.Messages;
+using FileAnalizer.Services;
+using FileAnalizer.Tools;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FileAnalizer.ViewModels;
 public class FileViewModel:ViewModelBase
@@ -30,9 +34,75 @@ public class FileViewModel:ViewModelBase
         get { return filetext; }
         set => base.PropertyChange(out filetext, value);
     }
-
+    private string? selectedItem;
+    public string? SelectedItem
+    {
+        get { return selectedItem; }
+        set => base.PropertyChange(out selectedItem, value);
+    }
     #endregion
 
+    #region Commands
+
+    private MyCommand? openCommand;
+    private MyCommand? saveCommand;
+
+    public MyCommand OpenCommand
+    {
+        get => this.openCommand ??= new MyCommand(
+            action: () => OpenFile(),
+            predicate: () => true);
+        set => base.PropertyChange(out this.openCommand, value);
+    }
+    public MyCommand SaveCommand
+    {
+        get => this.saveCommand ??= new MyCommand(
+            action: () => SaveChanges(),
+            predicate: () => true);
+        set => base.PropertyChange(out this.saveCommand, value);
+    }
+    #endregion
+
+    #region Methods
+    void OpenFile()
+    {
+        
+        if(string.IsNullOrEmpty(selectedItem))
+        {
+            return;
+        }
+        var thread = new Thread(() => {
+                try
+                { 
+                    FileText = string.Empty;
+                    using (StreamReader sr = new StreamReader(@$"App_Data\{selectedItem}"))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            this.FileText += line;
+                        }
+                    }
+                    return;
+                    
+                } 
+                catch {
+                    MessageBox.Show("No such file in directory");
+                }
+            
+        });
+        thread.Start();
+
+    }
+    void SaveChanges()
+    {
+        using (StreamWriter sw = new StreamWriter(@$"App_Data\{selectedItem}")) 
+        {
+            sw.WriteLine(this.FileText);
+        }
+
+    }
+    #endregion
 
 }
 
